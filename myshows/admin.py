2af35965322c5e4e7534dbada4804c8d8e284ef4
Astw <tekinoku@gmail.com>
2021-01-24 13:32:21 +0300

    added articles to admin

--- myshows/admin.py
+++ myshows/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Show, Poster, Genre, Tag
+from .models import Show, Poster, Genre, Tag, Article, ArticleImage
 
 admin.site.register(Show)
 admin.site.register(Poster)
+admin.site.register(ArticleImage)
 
 
 class GenresAdmin(admin.ModelAdmin):
@@ -12,7 +13,16 @@ class GenresAdmin(admin.ModelAdmin):
 
 admin.site.register(Genre, GenresAdmin)
 
+
 class TagsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title',)
 
+
 admin.site.register(Tag, TagsAdmin)
+
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+
+
+admin.site.register(Article, ArticlesAdmin)

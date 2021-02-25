cc1790112bad9e0ce93d35f3c5939ff2ac732f4a
Astw <tekinoku@gmail.com>
2021-02-26 01:41:08 +0300

    Hide my secrets

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xibz7ia08_j3gm6hcf-l)td5e5vu1dtsgjva33o2kc-gxuv#o$'
+with open('secret.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

0e51c4d43786b712637a9ed295e7c4a9bf12c6d5
Astw <tekinoku@gmail.com>
2021-02-27 00:54:06 +0300

    Use poster placeholder

--- myshows/models/show.py
+++ myshows/models/show.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.templatetags.static import static
 from django.utils.translation import gettext_lazy as _
 
 from myshows.models import Country, Genre, Tag, Network
@@ -75,6 +76,13 @@ class Show(models.Model):
 
     def directors(self):
         return self.personrole_set.filter(role=PersonRole.RoleType.DIRECTOR)
+
+    def get_poster(self):
+        poster = self.poster_set.first()
+        if poster:
+            return poster.image.url
+        else:
+            return static('poster_placeholder.jpg')
 
     @staticmethod
     def get_title_static(title):

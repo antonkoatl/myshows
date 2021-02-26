ca5ef5f27da172be68e08f256c64f8236b4ae8d3
Astw <tekinoku@gmail.com>
2021-02-26 16:09:36 +0300

    Added models

--- myshows/models/__init__.py
+++ myshows/models/__init__.py
@@ -1,1 +1,7 @@
 from .models import *
+from .article import Article, ArticleImage
+from .episode import Episode, EpisodeImage, EpisodeComment
+from .named_entity import NamedEntity, Lemma, NamedEntityOccurrence
+from .person import Person, PersonImage, PersonFact, PersonSpouse, PersonRole
+from .season import Season
+from .show import Show, Poster, Fact, Review, ShowVideo
